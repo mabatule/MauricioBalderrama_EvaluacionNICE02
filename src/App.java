e8f5008cf5d04730fc7337485c08dcdde2aab463
mabatule <maba1998a@gmail.com>
2022-10-26 15:17:53 -0400

    Solved exercise

--- src/App.java
+++ src/App.java
@@ -1,5 +1,52 @@
+import java.util.*;
 public class App {
+    public static String volvearPalabra(String cad) {
+        String cadNew="";
+        for (int i = cad.length()-1; i >=0 ; i--) {
+            cadNew+= cad.charAt(i);
+        }
+        return cadNew;
+    }
+    public static List<String> converPalabraTolist(String oracion){
+        String aux="";
+        List<String> listWord= new ArrayList<>();
+        
+        for (int i = 0; i < oracion.length(); i++) {
+            
+            if(oracion.charAt(i)==' '){
+                listWord.add(aux);
+                aux="";
+            }
+            aux+=oracion.charAt(i);
+        }
+        listWord.add(aux);
+
+        return listWord;
+    }
+    public static List<String> invertir5PalAdelante(List<String> oracion){
+        for (int i = 0; i < oracion.size(); i++) {
+            if(i>4){
+                oracion.set(i,volvearPalabra( oracion.get(i)) );
+            }
+        }
+        return oracion;
+    }
+    public static String convertOracion(List<String> oracion) {
+        String cad="";
+        for (int i = 0; i < oracion.size(); i++) {
+            cad+=oracion.get(i)+" ";
+        }
+        return cad;
+        
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<String> array;
+        String result,oracion;
+        Scanner sr= new Scanner(System.in);
+        oracion= sr.nextLine();
+        array = converPalabraTolist(oracion);
+        array = invertir5PalAdelante(array);
+        result= convertOracion(array);
+        System.out.println(result);
     }
 }
